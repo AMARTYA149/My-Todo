3024130edfea509857bd7e3eb45498a1b4f90eba
Amartya Aishwarya <amartya970914@gmail.com>
2018-12-14 15:18:40 +0530

    todos.js modified removing todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,11 @@
 $("li").click(function(){
   $(this).toggleClass("completed");
 });
+
+//removal of list item
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
